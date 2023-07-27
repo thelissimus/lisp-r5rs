6b6a8cba5c76381a1f27dc842bd1352167ed5cb8
thelissimus <thelissimus@tuta.io>
2023-07-27 23:17:29 +0500

    add: parsing atom, number, string

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,51 @@
+{-# LANGUAGE DerivingStrategies #-}
+
 module Lib where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+  deriving stock (Show, Eq)
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  pure $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  pure (String x)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseNumber <|> parseString
+
+readExpr :: String -> String
+readExpr s = case parse parseExpr "lisp" s of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value: " ++ show val
+
+main :: IO ()
+main = getLine >>= putStrLn . readExpr
